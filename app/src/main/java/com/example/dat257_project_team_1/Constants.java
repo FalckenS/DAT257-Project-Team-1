95455891ef1480d5e717edd5e10d523f740f615b
Falckishen <samuelfa@student.chalmers.se>
2023-04-13 20:47:22 +0200

    Added request for location permission

---
+++ app/src/main/java/com/example/dat257_project_team_1/Constants.java
@@ -0,0 +1,6 @@
+package com.example.dat257_project_team_1;
+
+public final class Constants {
+
+    public static int REQUEST_LOCATION_PERMISSION = 0;
+}
--- app/src/main/java/com/example/dat257_project_team_1/MainActivity.java
+++ app/src/main/java/com/example/dat257_project_team_1/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.dat257_project_team_1;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import static com.example.dat257_project_team_1.Constants.REQUEST_LOCATION_PERMISSION;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Check if the app has permission to access the user's location
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted, request the permission
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+        }
+
+        // Initialize the Google Places API client
+        Places.initialize(getApplicationContext(), apiKey);
+
+        // Create a new Places client instance
+        PlacesClient placesClient = Places.createClient(this);
+
+        // Get the user's current location
+        FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    // Use the location to search for recycling centers nearby
+                    searchForRecyclingCenters(location.getLatitude(), location.getLongitude());
+                }
+            }
+        });
+    }
+
+    // To handle the user's response to the permission request
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            // If request is cancelled, the result arrays are empty
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, do the location-related task you need to do
+            }
+            else {
+                // Permission denied, disable the location-related functionality
+            }
+        }
     }
 }
