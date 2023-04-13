63d422ce80d176a5f75ab53662e7ab9e2bf161e8
Algeskog <algeskog@student.chalmers.se>
2023-04-13 18:52:16 +0200

    Added a search bar and a search button plus added placeholder event listeners for both of them

--- app/src/main/java/com/example/dat257_project_team_1/MainActivity.java
+++ app/src/main/java/com/example/dat257_project_team_1/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.dat257_project_team_1;
 
+import android.content.Context;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.widget.AppCompatButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextInputEditText searchBar = (TextInputEditText) findViewById(R.id.searchBar);
+        // Placeholder search bar text field event listener
+        searchBar.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_ACTION_DONE){
+                    InputMethodManager imm = (InputMethodManager)textView.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(textView.getWindowToken(), 0);
+                    textView.clearFocus();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        AppCompatButton searchButton = (AppCompatButton) findViewById(R.id.searchButton);
+        // Placeholder search button event listener
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                    getCurrentFocus().clearFocus();
+                } catch (Exception e) {
+                    // TODO: handle exception
+                }
+            }
+        });
     }
 }
