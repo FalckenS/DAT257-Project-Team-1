5c822d52e5df72d8320b26875f43ac1748d2f7e0
Falckishen <samuelfa@student.chalmers.se>
2023-04-13 20:47:22 +0200

    Added request for location permission

--- app/src/main/java/com/example/dat257_project_team_1/MainActivity.java
+++ app/src/main/java/com/example/dat257_project_team_1/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.dat257_project_team_1;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+import static com.example.dat257_project_team_1.Constants.REQUEST_LOCATION_PERMISSION;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private boolean locationPermissionGranted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        requestLocationPermission();
+    }
+
+    private void requestLocationPermission() {
+        // Check if the app has permission to access the user's location
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted, request the permission
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+        }
+        else {
+            // Permission is already granted
+            locationPermissionGranted = true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted
+                locationPermissionGranted = true;
+            }
+            else {
+                // Permission denied
+                locationPermissionGranted = false;
+            }
+        }
     }
 }
