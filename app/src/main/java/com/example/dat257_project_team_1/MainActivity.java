7897409120a6120aaa79dc391e0c045e27cf1cdb
Falckishen <samuelfa@student.chalmers.se>
2023-04-18 18:28:53 +0200

    Get nearby recycling centers

--- app/src/main/java/com/example/dat257_project_team_1/MainActivity.java
+++ app/src/main/java/com/example/dat257_project_team_1/MainActivity.java
@@ -9,12 +9,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import com.google.android.gms.location.*;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.*;
+import com.google.android.libraries.places.api.model.*;
+import com.google.android.libraries.places.api.net.*;
+import com.google.android.libraries.places.api.Places;
+import okhttp3.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static com.example.dat257_project_team_1.Constants.*;
 
 public class MainActivity extends AppCompatActivity {
 
     private FusedLocationProviderClient fusedLocationClient;
+    private PlacesClient placesClient;
+    OkHttpClient okHttpClient;
     private boolean locationPermissionGranted = false;
     private Location currentLocation;
 
@@ -23,13 +39,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Places.initialize(getApplicationContext(), API_KEY);
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        okHttpClient = new OkHttpClient().newBuilder().build();
+        placesClient = Places.createClient(this);
 
         requestLocationPermission();
 
         if (locationPermissionGranted) {
             updateCurrentLocation();
         }
+
+        getRecyclingCenters();
     }
 
     private void requestLocationPermission() {
@@ -37,8 +58,7 @@ public class MainActivity extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // Permission is not granted, request the permission
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION_CODE);
-        }
-        else {
+        } else {
             // Permission is already granted
             locationPermissionGranted = true;
         }
@@ -51,8 +71,7 @@ public class MainActivity extends AppCompatActivity {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Permission granted
                 locationPermissionGranted = true;
-            }
-            else {
+            } else {
                 // Permission denied
                 locationPermissionGranted = false;
             }
@@ -86,5 +105,36 @@ public class MainActivity extends AppCompatActivity {
                     }
                 },
                 Looper.getMainLooper());
+    }
+
+    private void getRecyclingCenters() {
+        // https://developers.google.com/maps/documentation/places/web-service/search-nearby
+        MediaType mediaType = MediaType.parse("text/plain");
+        RequestBody body = RequestBody.create("", mediaType);
+        String coordinatesSouth = "";
+        String coordinatesEast = "";
+        String location = coordinatesSouth + "%2C" + coordinatesEast;
+        String keyword = "recycling center";
+        String opennow = "true";
+        String rankby = "distance";
+        String placesAPIUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" +
+                "&location=" + location +
+                "&keyword=" + keyword +
+                "&opennow=" + opennow +
+                "&rankby=" + rankby +
+                "&key=" + API_KEY;
+
+
+        Request request = new Request.Builder()
+                .url(placesAPIUrl)
+                .method("GET", body)
+                .build();
+        Response response;
+        try {
+            response = okHttpClient.newCall(request).execute();
+        }
+        catch (Exception e) {
+
+        }
     }
 }
