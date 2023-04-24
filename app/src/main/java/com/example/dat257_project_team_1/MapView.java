b281f419f90bfbde4510aebcdb81bf3ed577fe8e
houmam kadamani <houmam@student.chalmers.se>
2023-04-24 10:16:01 +0200

    map view created

---
+++ app/src/main/java/com/example/dat257_project_team_1/MapView.java
@@ -0,0 +1,15 @@
+package com.example.dat257_project_team_1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MapView extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_view);
+
+    }
+}
