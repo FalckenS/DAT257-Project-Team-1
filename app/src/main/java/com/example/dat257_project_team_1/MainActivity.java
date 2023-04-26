b4464397c7a178f3c010febc8f06f03365e36a82
Falckishen <samuelfa@student.chalmers.se>
2023-04-26 19:30:36 +0200

    Fixed current location, created ICurrentLocationTask

--- app/src/main/java/com/example/dat257_project_team_1/MainActivity.java
+++ app/src/main/java/com/example/dat257_project_team_1/MainActivity.java
@@ -11,23 +11,22 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import com.google.android.gms.location.*;
-import android.content.Intent;
-import android.view.View;
-import android.widget.ImageView;
-import static com.example.dat257_project_team_1.Constants.*;
 import androidx.appcompat.widget.AppCompatButton;
+import com.google.android.gms.location.*;
 import com.google.android.material.textfield.TextInputEditText;
+
+import static com.example.dat257_project_team_1.Constants.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean locationPermissionGranted;
-    private Location currentLocation;
+    private boolean coarseLocationPermissionGranted = false;
+    private boolean fineLocationPermissionGranted = false;
 
     private FusedLocationProviderClient fusedLocationClient;
     private PlacesAPIHandler placesAPIHandler;
@@ -57,29 +56,13 @@ public class MainActivity extends AppCompatActivity {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         placesAPIHandler = new PlacesAPIHandler();
 
-        requestLocationPermission();
-
-        // --- TEMP ---
-        locationPermissionGranted = false;
-        currentLocation = LocationFactory.createLocation(57.35494277871453, 12.126514588022303);
-        placesAPIHandler.updateRecyclingCenters(currentLocation);
-        // ------------
-
-        if (locationPermissionGranted) {
-            currentLocationInit();
-            while (currentLocation == null) {
-                // Wait for location
-            }
-            placesAPIHandler.updateRecyclingCenters(currentLocation);
-        }
-
         TextInputEditText searchBar = (TextInputEditText) findViewById(R.id.searchBar);
         // Placeholder search bar text field event listener
         searchBar.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
-                if (actionId == EditorInfo.IME_ACTION_DONE){
-                    InputMethodManager imm = (InputMethodManager)textView.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    InputMethodManager imm = (InputMethodManager) textView.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(textView.getWindowToken(), 0);
                     textView.clearFocus();
                     return true;
@@ -94,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 try {
-                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
                     getCurrentFocus().clearFocus();
                 } catch (Exception e) {
@@ -102,52 +85,63 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        requestLocationPermission();
+
+        if (coarseLocationPermissionGranted && fineLocationPermissionGranted) {
+            updateCurrentLocation(new ICurrentLocationTask() {
+                @Override
+                public void currentLocationTask(Location currentLocation) {
+                    placesAPIHandler.updateRecyclingCenters(currentLocation);
+                }
+            });
+        }
     }
 
     private void requestLocationPermission() {
-        locationPermissionGranted = false;
-        // Check if the app has permission to access the user's location
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // Permission is not granted, request the permission
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION_CODE);
+
+        coarseLocationPermissionGranted = false;
+        fineLocationPermissionGranted = false;
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, COARSE_LOCATION_PERMISSION_CODE);
         }
         else {
-            // Permission is already granted
-            locationPermissionGranted = true;
+            coarseLocationPermissionGranted = true;
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_PERMISSION_CODE);
+        }
+        else {
+            fineLocationPermissionGranted = true;
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NotNull String[] permissions, @NotNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_LOCATION_PERMISSION_CODE) {
-            locationPermissionGranted = grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
+        if (requestCode == COARSE_LOCATION_PERMISSION_CODE) {
+            coarseLocationPermissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+        }
+        if (requestCode == FINE_LOCATION_PERMISSION_CODE) {
+            fineLocationPermissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
         }
     }
 
-    private void currentLocationInit() {
-        LocationRequest locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, LOCATION_UPDATE_INTERVAL_MILLIS)
-                .setWaitForAccurateLocation(false)
-                .setMinUpdateIntervalMillis(MIN_LOCATION_UPDATE_MILLIS)
-                .setMaxUpdateDelayMillis(MAX_LOCATION_UPDATE_MILLIS)
-                .build();
+    private void updateCurrentLocation(ICurrentLocationTask currentLocationTask) {
 
-        // Another check for location permission, needed to use requestLocationUpdates. Ignore.
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestLocationPermission();
-            if (!locationPermissionGranted) {
-                return;
-            }
+            return;
         }
-
-        fusedLocationClient.requestLocationUpdates(locationRequest,
+        fusedLocationClient.requestLocationUpdates(
+                new LocationRequest.Builder(0)
+                        .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+                        .build(),
                 new LocationCallback() {
                     @Override
                     public void onLocationResult(@NotNull LocationResult locationResult) {
-                        Location lastLocation = locationResult.getLastLocation();
-                        if (lastLocation != null) {
-                            currentLocation = lastLocation;
-                        }
+                        currentLocationTask.currentLocationTask(locationResult.getLastLocation());
+                        fusedLocationClient.removeLocationUpdates(this);
                     }
                 },
                 Looper.getMainLooper());
