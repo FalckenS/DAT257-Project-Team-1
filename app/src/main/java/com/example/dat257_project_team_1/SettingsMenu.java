d05cb2667de2de50861ed1ec5479118aeedd4661
Phozel <adamwi@student.chalmers.se>
2023-05-04 13:43:52 +0200

    Tried to make a switch that changes the theme but failed to get it to apply

--- app/src/main/java/com/example/dat257_project_team_1/SettingsMenu.java
+++ app/src/main/java/com/example/dat257_project_team_1/SettingsMenu.java
@@ -1,7 +1,16 @@
 package com.example.dat257_project_team_1;
 
+import android.app.Application;
+import android.content.ContextWrapper;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Switch;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class SettingsMenu extends AppCompatActivity {
 
@@ -9,5 +18,17 @@ public class SettingsMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_menu);
+
+        Switch darkModeSwitch = findViewById(R.id.dark_mode_switch);
+
+        darkModeSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Click");
+            }
+        });
     }
+
+
+
 }
